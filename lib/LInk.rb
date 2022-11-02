0ba26953f92fdf7f0f90a9a3dddc9a29572185e6
hol3ng <savinyhal@gmail.com>
2022-11-03 00:30:13 +0300

    first

---
+++ lib/LInk.rb
@@ -0,0 +1,26 @@
+class Link < Post
+  def initialize
+    super
+    @url = ''
+  end
+
+  def read_from_console
+    puts 'Введите адрес ссылки'
+    @url = $stdin.gets.chomp
+
+    puts 'Напишите пару слов о том, куда ведёт ссылка'
+    @text = $stdin.gets.chomp
+  end
+
+  def save
+    file = File.new(file_path, 'w')
+    time_string = @created_at.strftime('%Y.%m.%d, %H:%M')
+    file.puts("#{time_string}\n\r")
+
+    file.puts(@url)
+    file.puts(@text)
+
+    file.close
+    puts 'Ваша ссылка сохранена'
+  end
+end
